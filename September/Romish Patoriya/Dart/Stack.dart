5f8e409136f628a3a818a4f22a51515b8c42757e
Romish Patoriya <patoriyaromish@gmail.com>
2021-09-07 19:31:42 +0530

    Update in Dart Format

--- September/Romish Patoriya/Dart/Stack.dart
+++ September/Romish Patoriya/Dart/Stack.dart
@@ -1,34 +1,30 @@
 //Stack Data Structure
 
-class Stack{
-  
+class Stack {
   var list = [];
-  peek(){
-    if(list.isNotEmpty){
-    print('${list[list.length-1]} was on top.');
-    }
-    else{
+  peek() {
+    if (list.isNotEmpty) {
+      print('${list[list.length - 1]} was on top.');
+    } else {
       print('Stack was Empty.');
     }
   }
-  
-  pop(){
-    if(list.isNotEmpty){
+
+  pop() {
+    if (list.isNotEmpty) {
       print('${list.removeLast()} was Remove.');
-    }
-    else{
+    } else {
       print('Stack was Empty.');
     }
   }
-  
-  push(var i){
+
+  push(var i) {
     list.add(i);
-    print('$i was added.' );
+    print('$i was added.');
   }
-  
 }
 
-void main(){
+void main() {
   Stack s = Stack();
   s.push(9);
   s.push(10);
