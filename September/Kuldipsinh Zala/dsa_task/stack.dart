fb9cdd516f3699f92a76c564de3b4a90341aa992
zalakuldip740 <zalakuldipsinh740@gmail.com>
2021-09-08 11:37:57 +0530

    issue : #3  Kuldipsinh Zala : Stack, Queue

--- September/Kuldipsinh Zala/dsa_task/stack.dart
+++ September/Kuldipsinh Zala/dsa_task/stack.dart
@@ -1,13 +1,13 @@
 class demo {
-  var stack = [1, 5, 8, 3, 9]; // this is growable list
+  List stack = []; // this is a growable list
   // consider stack has fix length of 5 for check stack is full or not
 
-  void push(int a) {
+  void push(var a) {
     if (stack.length == 5) {
       print('stack is full');
     } else {
-      stack.add(a);
-      print('after insertion:$stack');
+      stack.add(a); // inserted at last pos.
+      print('after insertion of $a element:$stack');
     }
   }
 
@@ -16,7 +16,7 @@ class demo {
     if (stack.isEmpty) {
       print('stack is empty');
     } else {
-      stack.removeAt(stack.length - 1);
+      stack.removeAt(stack.length - 1); // removed from the last pos.
       print('after deletion:$stack');
     }
   }
@@ -24,8 +24,17 @@ class demo {
 
 void main() {
   demo d = new demo();
-// insert an element
+// insert differet types of elemets
   d.push(12);
+  d.push('kuldip');
+  d.push(45.65);
   // remove an element
   d.pop();
 }
+
+// o/p
+// after insertion of 12 element:[12]
+// after insertion of kuldip element:[12, kuldip]
+// after insertion of 45.65 element:[12, kuldip, 45.65]
+// after deletion:[12, kuldip]
+
