2030ea747638b29cc0b63269270fc16476ce8afa
vivekmavani <vivekcmavani@gmail.com>
2021-09-17 09:56:48 +0530

    Retrofit Example By vivek

---
+++ September/Vivek Mavani/Flutter/RetrofitExample/lib/network/model/year_book_description.dart
@@ -0,0 +1,19 @@
+
+//part 'year_book_description.g.dart';
+class YearbookDescription{
+ final String Desc;
+ final String Price;
+
+ YearbookDescription({required this.Desc, required this.Price});
+
+ factory YearbookDescription.fromJson(Map<String, dynamic> json) =>
+     YearbookDescription(
+      Desc: json["Desc"],
+      Price: json["Price"],
+     );
+ Map<String, dynamic> toJson() => {
+  "Desc": Desc,
+  "Price": Price,
+ };
+
+}
