1d6f2cc9ba41d90835bdc2eb63ecfcd8ae63ea4b
Hiren Sanghani <84366746+hiren018@users.noreply.github.com>
2021-09-21 11:41:30 +0530

    Task6 : Reteofit_example

---
+++ September/Hiren Sanghani/Flutter-project/retrofit_prac/lib/api_service.dart
@@ -0,0 +1,22 @@
+import 'package:dio/dio.dart';
+import 'package:retrofit/http.dart';
+import 'package:retrofit_prac/data.dart';
+
+part 'api_service.g.dart';
+
+@RestApi(baseUrl: 'http://www.demoaws.picsy.in/api')
+abstract class ApiService {
+  factory ApiService(Dio dio, {String? baseUrl}) {
+    dio.options =
+        BaseOptions(receiveTimeout: 30000, connectTimeout: 30000, headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'Authorization': 'Bearer 950756f02948a64e992eaee4626a4818db59c4ed',
+    });
+
+    return _ApiService(dio, baseUrl: baseUrl);
+  }
+
+  @GET('/yearbook/getyearbook/28a6883292e53e1df2792d7fcb867108')
+  Future<Album> fetchAlbum();
+}
